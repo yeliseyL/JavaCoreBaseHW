f6da197a1aa79df083e73fa2de619efa13d5b368
Elisey Lobanov <crimsonorama@gmail.com>
2020-05-28 01:50:10 +0300

    hw5

--- src/HW5/Employee.java
+++ src/HW5/Employee.java
@@ -1,4 +1,33 @@
 package HW5;
 
 public class Employee {
+    private String fullName;
+    private String position;
+    private String email;
+    private String phoneNumber;
+    private int wage;
+    private int age;
+
+    public Employee(String fullName, String position, String email, String phoneNumber, int wage, int age) {
+        this.fullName = fullName;
+        this.position = position;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.wage = wage;
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void info() {
+        System.out.println("Name: " + fullName);
+        System.out.println("Position: " + position);
+        System.out.println("Email: " + email);
+        System.out.println("Phone: " + phoneNumber);
+        System.out.println("Wage: " + wage);
+        System.out.println("Age: " + age);
+        System.out.println();
+    }
 }
--- src/HW5/HomeWork5.java
+++ src/HW5/HomeWork5.java
@@ -1,4 +1,19 @@
 package HW5;
 
 public class HomeWork5 {
+    public static void main(String[] args) {
+        Employee[] employees = {
+                new Employee("Elliot Alderson", "Developer", "elliot@evilcorp.com", "555-111-11-11", 8000, 25),
+                new Employee("Tyrell Wellick", "CEO", "tyrell@evilcorp.com", "555-222-22-22", 15000, 43),
+                new Employee("Angela Moss", "Admin", "angela@evilcorp.com", "555-333-33-33", 7000, 31),
+                new Employee("Grace Gummer", "Head of Security", "grace@evilcorp.com", "555-444-44-44", 10000, 41),
+                new Employee("Gideon Goddard", "Lead Developer", "gideon@evilcorp.com", "555-555-55-55", 9000, 34),
+        };
+
+        for (Employee person : employees) {
+            if (person.getAge() > 40) {
+                person.info();
+            }
+        }
+    }
 }
