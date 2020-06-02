14a3b3d6ab381abc24558d03c9a9e4caf185e37b
Elisey Lobanov <crimsonorama@gmail.com>
2020-06-03 00:14:19 +0300

    hw6

---
+++ src/HW7/Cat.java
@@ -0,0 +1,14 @@
+package HW7;
+
+public class Cat {
+    private String name;
+    private int appetite;
+    public Cat(String name, int appetite) {
+        this.name = name;
+        this.appetite = appetite;
+    }
+    public void eat(Plate p) {
+        p.decreaseFood(appetite);
+    }
+}
+
---
+++ src/HW7/Main.java
@@ -0,0 +1,11 @@
+package HW7;
+
+public class Main {
+    public static void main(String[] args) {
+        Cat cat = new Cat("Barsik", 5);
+        Plate plate = new Plate(100);
+        plate.info();
+        cat.eat(plate);
+        plate.info();
+    }
+}
